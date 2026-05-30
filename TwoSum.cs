8933f72534c154e2af1941809ec1b9866be0059d
Саплюков Юрій <109668334+QuantGit43@users.noreply.github.com>
2026-05-30 15:32:07 +0300

    1.TwoSum

---
+++ TwoSum.cs
@@ -0,0 +1,19 @@
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var seen = new Dictionary<int, int>();
+ 
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+ 
+            if (seen.ContainsKey(complement))
+                return new[] { seen[complement], i };
+ 
+            seen[nums[i]] = i;
+        }
+ 
+        return Array.Empty<int>();
+    }
+}
